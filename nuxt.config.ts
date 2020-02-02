9e8daa382441744400c4d00c07c7e44683c023b6
1000ch <shogo.sensui@gmail.com>
2020-02-02 23:36:33 +0900

    chore: meta tweaks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,15 @@ const nuxtConfig: NuxtConfig = {
   mode: 'spa',
   srcDir: 'src',
   head: {
-    title: process.env.npm_package_name ?? '',
+    title: 'Twitter Search Query Builder',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description ?? '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description ?? '' },
+      { property: 'og:title', content: 'Twitter Search Query Builder' },
+      { property: 'og:description', content: 'GUI Tool to build a search query for Twitter' },
+      { property: 'og:url', content: 'https://twitter-search-query-builder.netlify.com/' },
+      { property: 'twitter:card', content: 'summary' }
     ]
   },
   buildModules: [
